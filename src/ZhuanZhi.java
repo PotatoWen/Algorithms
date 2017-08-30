110c35b28d3112857a09aa59d4ca323b5114b4d4
Potato Wen <wenxiao828@live.com>
2017-08-30 23:32:05 +0800

    Initial commit

---
+++ src/ZhuanZhi.java
@@ -0,0 +1,31 @@
+import edu.princeton.cs.algs4.StdRandom;
+
+public class ZhuanZhi {
+    public static void main(String args[]){
+
+        int M = StdRandom.uniform(10,20);
+        int N = StdRandom.uniform(10,20);
+        double[][] num = new double[M][N];
+        for (int i =0;i<M;i++) {
+            for (int j=0;j<N;j++) {
+                num[i][j] = StdRandom.uniform(100.0, 200.0);
+            }
+        }
+
+        for (int i=0;i<M;i++) {
+            for (int j=0;j<N;j++) {
+                System.out.printf("%4.2f"+" ",num[i][j]);
+            }
+            System.out.print('\n');
+        }
+
+        System.out.printf("二维数组的长宽是: %d,%d \n",M,N);
+
+        for (int i=0;i<N;i++) {
+            for (int j=0;j<M;j++) {
+                System.out.printf("%5.2f"+" ",num[j][i]);
+            }
+            System.out.print('\n');
+        }
+    }
+}
